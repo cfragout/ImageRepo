13a34a1d1c002fb306e2600ee504778fc1987d83
cfragout <cfragout@gmail.com>
2014-09-15 23:10:32 -0300

    Added sidebars

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
 	$('.image-container').click(function(){
-		$(this).toggleClass('selected');
+		var isSelected = $(this).hasClass('selected');
+
+		$('.image-container').removeClass('selected');
+		if (isSelected) {
+			$(this).removeClass('selected');
+			$('.actions span').addClass('fg-gray no-hover');
+			$('.actions').addClass('no-hover');
+		} else {
+			$('.actions span').removeClass('fg-gray no-hover');
+			$('.actions').removeClass('no-hover');
+			$(this).addClass('selected');
+		}
+
 	});
+
 
 });
