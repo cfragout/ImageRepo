cc0dd0908223b2c197ff1fbae1395c9d66436fe7
Federico <cf.barragan@gmail.com>
2014-10-14 16:24:38 -0300

    mmmmmm....

---
+++ BackEnd/ImagenesRepo/SourceCode/ImagenRepositorio/ImagenRepoEntities/Mappings/ImagenMap.cs
@@ -0,0 +1,22 @@
+﻿using ImagenRepoEntities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagenRepoEntities.Mappings
+{
+    public class ImagenMap : EntityTypeConfiguration<Imagen>
+    {
+        public ImagenMap()
+        {
+
+
+            this.HasMany(p => p.Tags)
+                .WithMany(p => p.Images);
+
+        }
+    }
+}
